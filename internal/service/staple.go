e9c15487259bf5566607972e503089b3310ba085
Gergely Brautigam <gergely@arangodb.com>
2019-12-30 20:36:46 +0100

    Typo

--- internal/service/staple.go
+++ internal/service/staple.go
@@ -26,7 +26,7 @@ type Staplerer interface {
 	Create(user *users.User) (staple *Staple, err error)
 	Delete(user *users.User, staple *Staple) (ok bool, err error)
 	Get(user *users.User) (staple *Staple, err error)
-	MarkSAsRead(user *users.User, staple *Staple) (err error)
+	MarkAsRead(user *users.User, staple *Staple) (err error)
 	List(user *users.User) (staples []*Staple, err error)
 	Archive(user *users.User, staple *Staple) (err error)
 }
