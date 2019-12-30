429b972ffd35c19f540b40cc42066a88b942455d
Gergely Brautigam <gergely@arangodb.com>
2019-12-30 21:20:42 +0100

    Users do not need to hold staples.

--- internal/users/user.go
+++ internal/users/user.go
@@ -1,10 +1,7 @@
 package users
 
-import "github.com/staple-org/staple/internal/service"
-
 // User defines a user of the system.
 type User struct {
-	Email   string
-	ID      string
-	Staples []*service.Staple
+	Email string
+	ID    string
 }
