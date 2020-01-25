8ab910889b66678ed80e064adfe91fbcd5bbc3b1
Gergely Brautigam <gergely@arangodb.com>
2020-01-25 23:07:52 +0100

    Added one more test

--- pkg/stapler_test.go
+++ pkg/stapler_test.go
@@ -145,3 +145,65 @@ func TestAddStaples(t *testing.T) {
 		assert.Equal(tt, "testcontent", staple.Staple.Content)
 	})
 }
+
+func TestDeleteStaples(t *testing.T) {
+	inMemoryStapleStore := storage.NewInMemoryStapleStorer()
+	stapleHandler := service.NewStapler(inMemoryStapleStore)
+	e := echo.New()
+	testUser := models.User{
+		Email:       "test@test.com",
+		Password:    "password",
+		ConfirmCode: "",
+		MaxStaples:  25,
+	}
+	stapleHandler.Create(models.Staple{
+		Name:      "TestStaple",
+		ID:        0,
+		Content:   "TestContent",
+		CreatedAt: time.Date(1981, 3, 28, 0, 0, 0, 0, time.UTC),
+		Archived:  false,
+	}, &testUser)
+
+	config.Opts.GlobalTokenKey = "test"
+	// Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["email"] = testUser.Email // from context
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token and send it as response.
+	tok, err := token.SignedString([]byte(config.Opts.GlobalTokenKey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Run("successful staple list", func(tt *testing.T) {
+		// Check if staple was created
+		req := httptest.NewRequest(echo.DELETE, "/rest/api/1/staple/0", bytes.NewBuffer([]byte("")))
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+tok)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetParamNames("id")
+		c.SetParamValues("0")
+		deleter := DeleteStaple(stapleHandler)
+		err = deleter(c)
+		assert.NoError(tt, err)
+		assert.Equal(tt, http.StatusOK, rec.Code)
+
+		// Check that it is really gone
+		req = httptest.NewRequest(echo.GET, "/rest/api/1/staple/0", bytes.NewBuffer([]byte("")))
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+tok)
+		rec = httptest.NewRecorder()
+		c = e.NewContext(req, rec)
+		c.SetParamNames("id")
+		c.SetParamValues("0")
+		getter := GetStaple(stapleHandler)
+		err = getter(c)
+		assert.NoError(tt, err)
+		assert.Equal(tt, http.StatusBadRequest, rec.Code)
+	})
+}
